a2290678cf4645857ef25b04dba27919b057e344
benna-mallory <69868279+benna-mallory@users.noreply.github.com>
2021-09-18 15:13:57 -0600

    Create BestOf3-PC03.py

---
+++ PC03/BestOf3-PC03.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 18:48:42 2021
+@author: 
+"""
+
+import turtle
+turtle.speed(10)
+turtle.colormode(255)
+turtle.bgcolor(0,0,0)
+
+turtle.penup()
+turtle.goto(-100,40)
+turtle.pendown()
+turtle.color("chartreuse")
+
+
+for iteration in range (0,37):
+    turtle.forward(200)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(-120,190)
+turtle.color("DeepPink1")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(200)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(-90,-250)
+turtle.color("violet")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(150)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(100,280)
+turtle.color("yellow")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(150)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(-305,-160)
+turtle.color("cyan")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(200)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(290,-160)
+turtle.color("white")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(150)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(205,120)
+turtle.color("MediumPurple1")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(170)
+    turtle.right(170)
+    
+turtle.penup()
+turtle.goto(275,40)
+turtle.color("blue")
+turtle.pendown()
+
+    
+for iteration in range (0,37):
+    turtle.forward(120)
+    turtle.right(170)
